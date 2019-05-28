2850b785786fab6d5d8c270e1accfdfe260f9022
AlzbetaSlavikova <bet.slavikova@gmail.com>
2019-05-28 14:05:55 +0200

    doplnění hodnot tech.kapacit, roztažení jejich linky přes celý graf

--- Actual_1.2/main.py
+++ Actual_1.2/main.py
@@ -19,16 +19,24 @@ class MultiCheckboxField(SelectMultipleField):
   widget= widgets.ListWidget(prefix_label=False)
   option_widget= widgets.CheckboxInput()
 
+class OperatorDetail(FlaskForm):
+  operator_id=SelectField(u'Operator', coerce = int)
+
+def edit_point(request, id):
+  point = Point.query.get(id)
+  form = OperatorDetail(request.POST, obj=point)
+  form.operator_id.choices = [(g.id, g.name) for g in Operator.query.order_by('name')]
+
 class EFormular(FlaskForm):
-    operator = SelectField("Operátor", choices=[("SK-TSO-0001", "eustream"),("BE-TSO-0001", "Fluxys Belgium"),("DE-TSO-0001", "Gascade"),("AT-TSO-0001", "Gas Connect Austria"),("PL-TSO-0001", "Gaz-System"),("CZ-TSO-0001", "Moravia GS"),("CZ-TSO-0001", "NET4GAS"),("DE-TSO-0009" ,"Open Grid Europe"),("DE-TSO-0003", "ONTRAS"),("DE-TSO-0016", "OPAL"),("IT-TSO-0001", "Snam Rete Gas"),("AT-TSO-0003","TAG"),("UA-TSO-0001", "Ukrtransgaz"),("DE-TSO-0001","Gastransport")])
-    point = SelectField("IP", choices=[("Baumgarten","Baumgarten"),("Brandov STEGAL (CZ) / Stegal (DE)","Brandov STEGAL"),("Brandov-OPAL (DE)","Brandov OPAL"),("Waidhaus" ,"Waidhaus"),("Lanžhot" ,"Lanžhot"),("Hora Svaté Kateřiny (CZ) / Deutschneudorf (Sayda) (DE)","HSK/Deutschendorf"),("Oberkappel (OGE)","Oberkappel"),("Olbernhau (DE) / Hora Svaté Kateřiny (CZ)","Olberhau/HSK"),("Kondratki","Kondratki"),("Mallnow","Mallnow"),("Tarvisio (IT) / Arnoldstein (AT)","Tarvisio/Arnoldstein"), ("Uzhgorod (UA) - Velké Kapušany (SK)","Užhorod/Velké Kapušany"),("VGS Moravia", "Moravia")])
+    operator = SelectField("Operátor", choices=[("SK-TSO-0001", "eustream"),("DE-TSO-0001", "Gascade"),("AT-TSO-0001", "Gas Connect Austria"),("PL-TSO-0001", "Gaz-System"),("CZ-TSO-0001", "Moravia GS"),("CZ-TSO-0001", "NET4GAS"),("DE-TSO-0009" ,"Open Grid Europe"),("DE-TSO-0003", "ONTRAS"),("DE-TSO-0016", "OPAL"),("IT-TSO-0001", "Snam Rete Gas"),("AT-TSO-0003","TAG"),("UA-TSO-0001", "Ukrtransgaz"),("DE-TSO-0001","Gastransport")])
+    point = SelectField("IP", choices=[("Baumgarten","Baumgarten"),("Brandov STEGAL (CZ) / Stegal (DE)","Brandov STEGAL"),("Brandov-OPAL (DE)","Brandov OPAL"),("Waidhaus" ,"Waidhaus"),("Lanžhot" ,"Lanžhot"),("Hora Svaté Kateřiny (CZ) / Deutschneudorf (Sayda) (DE)","HSK/Deutschendorf"),("Olbernhau (DE) / Hora Svaté Kateřiny (CZ)","Olberhau/HSK"),("Kondratki","Kondratki"),("Mallnow","Mallnow"),("Tarvisio (IT) / Arnoldstein (AT)","Tarvisio/Arnoldstein"), ("Uzhgorod (UA) - Velké Kapušany (SK)","Užhorod/Velké Kapušany"),("VGS Moravia", "Moravia"),("Cieszyn (PL) / Český Těšín (CZ)","Český Těšín")])
     indicator = MultiCheckboxField("Indikátor", choices=[("Interruptible Available" ,"Interruptible Available Capacity"),("Interruptible Booked", "Interruptible Booked Capacity"),("Interruptible Total" ,"Interruptible Total Capacity"),("Firm Technical", "Firm Technical Capacity"),("Firm Booked", "Firm Booked Capacity"),("Firm Available", "Firm Available Capacity"),("Planned interruption of firm capacity", "Planned interruption of firm capacity"),("Unplanned interruption of firm capacity", "Unplanned interruption of firm capacity"),("Planned interruption of interruptible capacity", "Planned interruption of interruptible capacity"), ("Unplanned interruption of interruptible capacity", "Unplanned interruption of interruptible capacity")])
     date_from = DateField("Datum od", format='%Y-%m-%d')
     date_to = DateField("Datum do", format='%Y-%m-%d')
 
 class IFormular(FlaskForm):
-    operator = SelectField("Operátor", choices=[("SK-TSO-0001", "eustream"),("BE-TSO-0001", "Fluxys Belgium"),("DE-TSO-0001", "Gascade"),("AT-TSO-0001", "Gas Connect Austria"),("PL-TSO-0001", "Gaz-System"),("CZ-TSO-0001", "Moravia GS"),("CZ-TSO-0001", "NET4GAS"),("DE-TSO-0009" ,"Open Grid Europe"),("DE-TSO-0003", "ONTRAS"),("DE-TSO-0016", "OPAL"),("IT-TSO-0001", "Snam Rete Gas"),("AT-TSO-0003","TAG"),("UA-TSO-0001", "Ukrtransgaz")])
-    point = SelectField("IP", choices=[("Baumgarten","Baumgarten"),("Brandov STEGAL (CZ) / Stegal (DE)","Brandov STEGAL"),("Brandov-OPAL (DE)","Brandov OPAL"),("Waidhaus" ,"Waidhaus"),("Lanžhot" ,"Lanžhot"),("Hora Svaté Kateřiny (CZ) / Deutschneudorf (Sayda) (DE)","HSK/Deutschendorf"),("Oberkappel (OGE)","Oberkappel"),("Olbernhau (DE) / Hora Svaté Kateřiny (CZ)","Olberhau/HSK"),("Kondratki","Kondratki"),("Mallnow","Mallnow"),("Tarvisio (IT) / Arnoldstein (AT)","Tarvisio/Arnoldstein"), ("Uzhgorod (UA) - Velké Kapušany (SK)","Užhorod/Velké Kapušany")])
+    operator = SelectField("Operátor", choices=[("SK-TSO-0001", "eustream"),("DE-TSO-0001", "Gascade"),("AT-TSO-0001", "Gas Connect Austria"),("PL-TSO-0001", "Gaz-System"),("CZ-TSO-0001", "Moravia GS"),("CZ-TSO-0001", "NET4GAS"),("DE-TSO-0009" ,"Open Grid Europe"),("DE-TSO-0003", "ONTRAS"),("DE-TSO-0016", "OPAL"),("IT-TSO-0001", "Snam Rete Gas"),("AT-TSO-0003","TAG"),("UA-TSO-0001", "Ukrtransgaz")])
+    point = SelectField("IP", choices=[("Baumgarten","Baumgarten"),("Brandov STEGAL (CZ) / Stegal (DE)","Brandov STEGAL"),("Brandov-OPAL (DE)","Brandov OPAL"),("Waidhaus" ,"Waidhaus"),("Lanžhot" ,"Lanžhot"),("Hora Svaté Kateřiny (CZ) / Deutschneudorf (Sayda) (DE)","HSK/Deutschendorf"),("Olbernhau (DE) / Hora Svaté Kateřiny (CZ)","Olberhau/HSK"),("Kondratki","Kondratki"),("Mallnow","Mallnow"),("Tarvisio (IT) / Arnoldstein (AT)","Tarvisio/Arnoldstein"), ("Uzhgorod (UA) - Velké Kapušany (SK)","Užhorod/Velké Kapušany"),("VGS Moravia", "Moravia"),("Cieszyn (PL) / Český Těšín (CZ)","Český Těšín")])
     direction = SelectField("Entry/Exit", choices=[("entry", "Entry"),("exit", "Exit")])
     indicator = MultiCheckboxField("Indikátor", choices=[("Interruptible Available" ,"Interruptible Available Capacity"),("Interruptible Booked", "Interruptible Booked Capacity"),("Interruptible Total" ,"Interruptible Total Capacity"),("Firm Technical", "Firm Technical Capacity"),("Firm Booked", "Firm Booked Capacity"),("Firm Available", "Firm Available Capacity"),("Planned interruption of firm capacity", "Planned interruption of firm capacity"),("Unplanned interruption of firm capacity", "Unplanned interruption of firm capacity"),("Planned interruption of interruptible capacity", "Planned interruption of interruptible capacity"), ("Unplanned interruption of interruptible capacity", "Unplanned interruption of interruptible capacity")])
     date_from = DateField("Datum od", format='%Y-%m-%d')
@@ -63,7 +71,9 @@ def index2():
 def plot():
     return render_template("plot.html")  
 
-
+@app.route("/plot2", methods = ["GET"])
+def plot2():
+    return render_template("plot2.html")
 # Když prohlížeč požádá o zobrazení obrázku plot.png, tak se zavolá tahle route,
 # ve které my obrázek s grafem vygenerujeme
 @app.route("/plot.png", methods = ["GET"])
@@ -159,58 +169,79 @@ def render_plot():
           if datum >= hodnota['periodFrom'] and datum <= hodnota['periodTo']:
               hodnoty_exit.append(hodnota['value'])
               break #ukončí podmínku pokud je splněna a vrátí se na začátek
-    #hodnoty technických kapacit na jednotlivých bodech, operátorech a směrech - musíme nějak domyslet, jak z toho udělat dlouhou linku přes celý graf a ne jeden bod
+
+  #seznam technických kapacit - pevné po celý rok  
     technical = [
-      {'operatorLabel':'NET4GAS','pointLabel':'Lanžhot','directionKey':'entry','value':-1640413000},
-      {'operatorLabel':'NET4GAS','pointLabel':'Lanžhot','directionKey':'exit', 'value':913680000},{'operatorLabel':'eustream','pointLabel':'Lanžhot','directionKey':'entry','value':-696800000},
-      {'operatorLabel':'eustream','pointLabel':'Lanžhot','directionKey':'exit','value':400400000},
-      {'operatorLabel':'NET4GAS','pointLabel':'Waidhaus','directionKey':'entry','value':-120000000},
-      {'operatorLabel':'NET4GAS','pointLabel':'Waidhaus','directionKey':'exit','value':1071742000},
-      {'operatorLabel':'Open Grid Europe','pointLabel':'Waidhaus','directionKey':'entry','value':0},
-      {'operatorLabel':'Open Grid Europe','pointLabel':'Waidhaus','directionKey':'exit','value':906900000},
-      {'operatorLabel':'NET4GAS','pointLabel':'Olbernhau','directionKey':'entry','value':-367000000},
-      {'operatorLabel':'NET4GAS','pointLabel':'Olbernhau','directionKey':'exit','value':0},
-      {'operatorLabel':'Gascade','pointLabel':'Olbernhau','directionKey':'entry','value':-367000000},
-      {'operatorLabel':'Gascade','pointLabel':'Olbernhau','directionKey':'exit','value':325090000},
-      {'operatorLabel':'NET4GAS','pointLabel':'STEGAL','directionKey':'entry','value':0},
-      {'operatorLabel':'NET4GAS','pointLabel':'STEGAL','directionKey':'exit','value':290136000},
-      {'operatorLabel':'Gascade','pointLabel':'STEGAL','directionKey':'entry','value':-302670000},
-      {'operatorLabel':'Gascade','pointLabel':'STEGAL','directionKey':'exit','value':0},
-      {'operatorLabel':'NET4GAS','pointLabel':'OPAL','directionKey':'entry','value':-1104838000},
-      {'operatorLabel':'NET4GAS','pointLabel':'OPAL','directionKey':'exit','value':0},
-      {'operatorLabel':'Gascade','pointLabel':'OPAL','directionKey':'entry','value':0},
-      {'operatorLabel':'Gascade','pointLabel':'OPAL','directionKey':'exit','value':761498000},
-      {'operatorLabel':'Gascade','pointLabel':'Mallnow','directionKey':'entry','value':-931500000},
-      {'operatorLabel':'Gascade','pointLabel':'Mallnow','directionKey':'exit','value':0},
-      {'operatorLabel':'Gaz-system','pointLabel':'Kondratki','directionKey':'entry','value':-1024300000},
-      {'operatorLabel':'Gaz-system','pointLabel':'Kondratki','directionKey':'exit','value':0},
-      {'operatorLabel':'Moravia GS','pointLabel':'Moravia','directionKey':'entry','value':-53675000},
-      {'operatorLabel':'Moravia GS','pointLabel':'Moravia','directionKey':'exit','value':89270000},
-      ]      
+      {'operatorKey':'CZ-TSO-0001','pointLabel':'Lanžhot','directionKey':'entry','value':-1640413000},
+      {'operatorKey':'CZ-TSO-0001','pointLabel':'Lanžhot','directionKey':'exit', 'value':913680000},
+      {'operatorKey':'SK-TSO-0001','pointLabel':'Lanžhot','directionKey':'entry','value':-696800000},
+      {'operatorKey':'SK-TSO-0001','pointLabel':'Lanžhot','directionKey':'exit','value':400400000},
+      {'operatorKey':'CZ-TSO-0001','pointLabel':'Waidhaus','directionKey':'entry','value':-120000000},
+      {'operatorKey':'CZ-TSO-0001','pointLabel':'Waidhaus','directionKey':'exit','value':1071742000},
+      {'operatorKey':'DE-TSO-0009','pointLabel':'Waidhaus','directionKey':'entry','value':0},
+      {'operatorKey':'DE-TSO-0009','pointLabel':'Waidhaus','directionKey':'exit','value':906900000},
+      {'operatorKey':'CZ-TSO-0001','pointLabel':'Olbernhau (DE) / Hora Svaté Kateřiny (CZ)','directionKey':'entry','value':-367000000},
+      {'operatorKey':'CZ-TSO-0001','pointLabel':'Olbernhau (DE) / Hora Svaté Kateřiny (CZ)','directionKey':'exit','value':0},
+      {'operatorKey':'DE-TSO-0001','pointLabel':'Olbernhau (DE) / Hora Svaté Kateřiny (CZ)','directionKey':'entry','value':-367000000},
+      {'operatorKey':'DE-TSO-0001','pointLabel':'Olbernhau (DE) / Hora Svaté Kateřiny (CZ)','directionKey':'exit','value':325090000},
+      {'operatorKey':'CZ-TSO-0001','pointLabel':'Brandov STEGAL (CZ) / Stegal (DE)','directionKey':'entry','value':0},
+      {'operatorKey':'CZ-TSO-0001','pointLabel':'Brandov STEGAL (CZ) / Stegal (DE)','directionKey':'exit','value':290136000},
+      {'operatorKey':'DE-TSO-0001','pointLabel':'Brandov STEGAL (CZ) / Stegal (DE)','directionKey':'entry','value':-302670000},
+      {'operatorKey':'DE-TSO-0001','pointLabel':'Brandov STEGAL (CZ) / Stegal (DE)','directionKey':'exit','value':0},
+      {'operatorKey':'DE-TSO-0001','pointLabel':'Mallnow','directionKey':'entry','value':-931500000},
+      {'operatorKey':'DE-TSO-0001','pointLabel':'Mallnow','directionKey':'exit','value':0},
+      {'operatorKey':'PL-TSO-0001','pointLabel':'Kondratki','directionKey':'entry','value':-1024300000},
+      {'operatorKey':'PL-TSO-0001','pointLabel':'Kondratki','directionKey':'exit','value':0},
+      {'operatorKey':'CZ-TSO-0001','pointLabel':'Moravia','directionKey':'entry','value':-53675000},
+      {'operatorKey':'CZ-TSO-0001','pointLabel':'Moravia','directionKey':'exit','value':89270000},
+      {'operatorKey':'CZ-TSO-0001','pointLabel':'Cieszyn (PL) / Český Těšín (CZ)','directionKey':'entry','value':0},
+      {'operatorKey':'CZ-TSO-0001','pointLabel':'Cieszyn (PL) / Český Těšín (CZ)','directionKey':'exit','value':28052000},
+      {'operatorKey':'PL-TSO-0001','pointLabel':'Cieszyn (PL) / Český Těšín (CZ)','directionKey':'entry','value':0},
+      {'operatorKey':'PL-TSO-0001','pointLabel':'Cieszyn (PL) / Český Těšín (CZ)','directionKey':'exit','value':0},
+      {'operatorKey':'CZ-TSO-0001','pointLabel':'Hora Svaté Kateřiny (CZ) / Deutschneudorf (Sayda) (DE)','directionKey':'entry','value':-150900000},
+      {'operatorKey':'CZ-TSO-0001','pointLabel':'Hora Svaté Kateřiny (CZ) / Deutschneudorf (Sayda) (DE)','directionKey':'exit','value':197530000},
+      {'operatorKey':'DE-TSO-0003','pointLabel':'Hora Svaté Kateřiny (CZ) / Deutschneudorf (Sayda) (DE)','directionKey':'entry','value':-197500000},
+      {'operatorKey':'DE-TSO-0003','pointLabel':'Hora Svaté Kateřiny (CZ) / Deutschneudorf (Sayda) (DE)','directionKey':'exit','value':135300000},
+      {'operatorKey':'AT-TSO-0001','pointLabel':'Baumgarten','directionKey':'entry','value':-477768000},
+      {'operatorKey':'AT-TSO-0001','pointLabel':'Baumgarten','directionKey':'exit','value':246528000},
+      {'operatorKey':'SK-TSO-0001','pointLabel':'Baumgarten','directionKey':'entry','value':-247520000},
+      {'operatorKey':'SK-TSO-0001','pointLabel':'Baumgarten','directionKey':'exit','value':1570400000},
+      {'operatorKey':'AT-TSO-0003','pointLabel':'Baumgarten','directionKey':'entry','value':-1436064000},
+      {'operatorKey':'AT-TSO-0003','pointLabel':'Baumgarten','directionKey':'exit','value':0},
+      {'operatorKey':'SK-TSO-0001','pointLabel':'Uzhgorod (UA) - Velké Kapušany (SK)','directionKey':'entry','value':-2028000000},
+      {'operatorKey':'SK-TSO-0001','pointLabel':'Uzhgorod (UA) - Velké Kapušany (SK)','directionKey':'exit','value':0},
+      {'operatorKey':'UA-TSO-0001','pointLabel':'Uzhgorod (UA) - Velké Kapušany (SK)','directionKey':'entry','value':0},
+      {'operatorKey':'UA-TSO-0001','pointLabel':'Uzhgorod (UA) - Velké Kapušany (SK)','directionKey':'exit','value':2080000000},
+      {'operatorKey':'IT-TSO-0001','pointLabel':'Tarvisio (IT) / Arnoldstein (AT)','directionKey':'entry','value':-1158796000},
+      {'operatorKey':'IT-TSO-0001','pointLabel':'Tarvisio (IT) / Arnoldstein (AT)','directionKey':'exit','value':0},
+      {'operatorKey':'AT-TSO-0003','pointLabel':'Tarvisio (IT) / Arnoldstein (AT)','directionKey':'entry','value':0},
+      {'operatorKey':'AT-TSO-0003','pointLabel':'Tarvisio (IT) / Arnoldstein (AT)','directionKey':'exit','value':1200359000},
+      {'operatorKey':'DE-TSO-0016','pointLabel':'Brandov-OPAL (DE)','directionKey':'entry','value':0},
+      {'operatorKey':'DE-TSO-0016','pointLabel':'Brandov-OPAL (DE)','directionKey':'exit','value':761498000},
+      {'operatorKey':'CZ-TSO-0001','pointLabel':'Brandov-OPAL (DE)','directionKey':'entry','value':-1104838000},
+      {'operatorKey':'CZ-TSO-0001','pointLabel':'Brandov-OPAL (DE)','directionKey':'exit','value':0}
+        ]      
 
     technical_capacity_exit = []
-    if response_2.status_code == 200:
-      for hodnota in seznam_exit:
+    #if response_1.status_code == 200 or response_2.status_code == 200 or response_1.status_code == 404 or response_2.status_code == 404:
+    for datum in datumy:
         for i in technical: 
-          if hodnota['operatorLabel'] == i['operatorLabel'] and hodnota['pointLabel'] == i['pointLabel'] and hodnota['directionKey'] == i['directionKey']:
+          if i['operatorKey'] == operator and i['pointLabel'] == point and i['directionKey'] == 'exit':
             technical_capacity_exit.append(i['value'])
-     
+   
     technical_capacity_entry = []
-    if response_1.status_code == 200:
-      for hodnota in seznam_entry:
+    #if response_1.status_code == 200 or response_2.status_code == 200 or response_1.status_code == 404 or response_2.status_code == 404:
+    for datum in datumy:
         for i in technical: 
-          if hodnota['operatorLabel'] == i['operatorLabel'] and hodnota['pointLabel'] == i['pointLabel'] and hodnota  ['directionKey'] == i['directionKey']:
+          if i['operatorKey'] == operator and i['pointLabel'] == point and i['directionKey'] == 'entry':
             technical_capacity_entry.append(i['value'])
-    
-      #for hodnota in seznam_entry:        #zatím nevím, proč nefunguje zadání obojího
-      #  if hodnota['operatorLabel'] == 'Open Grid Europe' and hodnota['pointLabel']== 'Waidhaus':
-       #     technical_capacity_entry.append('906900000')
+
           
 
     return render_template('chart.html', data_rows = zip(datumy, hodnoty_entry, hodnoty_exit, technical_capacity_exit,  technical_capacity_entry))
  
 
-@app.route("/plot.png", methods = ["GET"])
+@app.route("/plot2.png", methods = ["GET"])
 def render_plot_I():
     # import pro graf I (druhý)
     operator = request.args.get("operator")
@@ -247,23 +278,6 @@ def render_plot_I():
             "pointLabel": x['pointLabel'],
             "pointKey": x['pointKey']}
         seznam.append(hodnoty)
-
-  #  for index, req in enumerate(requests):
-  #    method = req['method']
-  #    path = req['path']
-  #    body = req.get('body', None)
-
-  #    with app.app_context(path, method=method, data = body):
-   #     try:
-    #      rv=app.preprocess_request()
-     #     if rv is None:
-      #      rv = app.dispatch_request() 
-       # except Exception as e:
-      #    rv = app. handle_user_exception(e)
-       # response = app.make_response(rv)
-    #    response = app.process_response(response)
-    #  response.append({"status": response.status_code, "response": _read_response(response)})
-   # return make_response(json.dumps(responses), 207, HEADERS)
 
 # vytvoříme seznam všech datumů, které jsou v zadaném období, je možné použít pro oba API call
     
